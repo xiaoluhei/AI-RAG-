53b619d6236dfee00e15b633adbc28a048f928b7
luzj <3235008722@qq.com>
2026-02-14 15:45:36 +0800

    第三章

---
+++ P3_LangChainRAG开发/02LangChain访问阿里云通义千问大模型.py
@@ -0,0 +1,9 @@
+from langchain_community.llms.tongyi import Tongyi
+
+# 不用qwen3-max，因为qwen3-max是聊天模型，qwen-max是大语言模型
+model = Tongyi(model="qwen-max")
+
+# 调用invoke方法向模型提问
+res = model.invoke(input="你是谁呀能做什么？")
+
+print(res)
